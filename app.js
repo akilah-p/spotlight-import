d03ae627c8d91d3ba65521ec5d46359a4346dc2a
Akilah Powell <atpjun@gmail.com>
2022-11-17 18:13:41 -0400

    created render function and event listnerer for first domain

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 /* Imports */
+import { fetchStock } from './fetch-utils.js';
+import { renderStock } from './render-utils.js';
 
 /* Get DOM Elements */
+const stockContainer = document.getElementById('stock-list');
 
-/* State */
 
 /* Events */
+window.addEventListener('load', async () => {
+    const stocks = await fetchStock();
+    console.log(stocks);
+
+    for (let stock of stocks) {
+        const stockEl = renderStock(stock);
+        stockContainer.append(stockEl);
+    }
+
+});
 
 /* Display Functions */
 
--- render-utils.js
+++ render-utils.js
@@ -1,1 +1,37 @@
-export function render
+export function renderStock(stockObject) {
+    const div = document.createElement('div');
+
+    const h2 = document.createElement('h2');
+    h2.textContent = stockObject.name;
+
+    const p = document.createElement('p');
+    p.textContent = `${stockObject.name} is in ${stockObject.market} and has a cap of ${stockObject.caps}`;
+
+    div.append(h2, p);
+    return div;
+
+}
+
+
+
+// export function renderAnimal(animalObject) {
+//     const div = document.createElement('div');
+
+//     const h2 = document.createElement('h2');
+//     h2.textContent = animalObject.name;
+
+//     const p = document.createElement('p');
+//     p.textContent = `${animalObject.name} is in ${stockObject.market}`;
+
+
+
+
+//     const ul = document.createElement('ul');
+//     for (let cap of stockObject.caps) {
+//         const li = document.createElement('li');
+//         li.textContent = cap;
+//         ul.append(li);
+//     }
+//     div.append(h2, p, ul);
+//     return div;
+// }
